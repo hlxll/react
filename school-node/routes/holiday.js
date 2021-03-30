55dc69da423961363e5838393ebe4de05b0bd1c5
黄林19 <huanglin19@hikvision.com>
2021-03-30 10:49:04 +0800

    '接口更新'

---
+++ school-node/routes/holiday.js
@@ -0,0 +1,25 @@
+var expiress = require('express')
+var router = expiress.Router()
+router.get('/searchTicket', function (req, res) {
+  var data = req.query;
+  var MongoClient = require("mongodb").MongoClient;
+  var url = "mongodb://localhost:27017";
+  MongoClient.connect(
+    url,
+    { useUnifiedTopology: true, useNewUrlParser: true },
+    function (err, db) {
+      if (err) throw err;
+      var dbo = db.db("admin");
+      var whereStr = data; // 查询条件
+      //find是查询条件，limit是返回条数
+      dbo
+        .collection("holiday")
+        .find(whereStr)
+        .toArray(function (err, result) {
+          if (err) throw err;
+          db.close();
+          res.send(result);
+        });
+    }
+  );
+})
--- school-node/routes/users.js
+++ school-node/routes/users.js
@@ -1,7 +1,7 @@
 var express = require("express");
 var router = express.Router();
 
-/* GET users listing. */
+//登录
 router.get("/login", function (req, res, next) {
   var data = req.query;
   var MongoClient = require("mongodb").MongoClient;
@@ -17,7 +17,7 @@ router.get("/login", function (req, res, next) {
       dbo
         .collection("user")
         .find(whereStr)
-        .limit(10)
+        .limit(1)
         .toArray(function (err, result) {
           if (err) throw err;
           db.close();
@@ -48,4 +48,63 @@ router.get("/login", function (req, res, next) {
     }
   );
 });
+//注册
+app.get('/register', function (req, res) {
+  var data = req.query;
+  console.log(data)
+  var MongoClient = require('mongodb').MongoClient;
+  var url = "mongodb://localhost:27017";
+
+  MongoClient.connect(url, { useNewUrlParser: true }, function (err, db) {
+    if (err) throw err;
+    var dbo = db.db("admin");
+    var myobj = data;
+    dbo.collection("user").insertOne(myobj, function (err, res) {
+      if (err) throw err;
+      console.log("文档插入成功");
+      db.close();
+    });
+  });
+  res.send('成功注册');
+})
+//查看订单
+app.get('/queryOrder', function (req, res) {
+  var data = req.query;
+  var MongoClient = require('mongodb').MongoClient;
+  var url = 'mongodb://localhost:27017';
+  MongoClient.connect(url, { useUnifiedTopology: true, useNewUrlParser: true }, function (err, db) {
+    if (err) throw err;
+    var dbo = db.db("admin");
+    var whereStr = data;  // 查询条件
+    //find是查询条件，limit是返回条数
+    dbo.collection("orderList").find(whereStr).limit(10).toArray(function (err, result) {
+      if (err) throw err;
+      db.close();
+      console.log(result)
+      res.send(result)
+    });
+  })
+})
+//添加订单
+app.get('/addOrder', function (req, res) {
+  var data = req.query;
+  var MongoClient = require('mongodb').MongoClient;
+  var url = "mongodb://localhost:27017/";
+  MongoClient.connect(url, { useUnifiedTopology: true, useNewUrlParser: true }, function (err, db) {
+    if (err) throw err;
+    var dbo = db.db("admin");
+    var duplicate = {
+      type: "1",
+      time: "2020-03-05",
+      money: "216",
+      name: "西湖一日游"
+    }
+    dbo.collection("orderList").find(duplicate).toArray(function (err, result) {
+      if (err) throw err;
+      db.close();
+      res.send('添加成功')
+    });
+  })
+
+})
 module.exports = router;
