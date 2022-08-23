7226c8b179628d71cd24317cb178620cbbd08a33
hlxll <447404195@qq.com>
2022-08-23 17:24:11 +0800

    ws请求

--- school-node/app.js
+++ school-node/app.js
@@ -143,4 +143,38 @@ app.use(function (err, req, res, next) {
   res.render("error");
 });
 
+
+
+/**
+ * 实现ws通信
+ * 
+*/
+var ws = require("nodejs-websocket")
+let chatA = null
+let chatB = null
+ws.createServer(function (conn) {
+  conn.on("text", function (msg) {
+    if (chatA && chatB) {
+      if (conn === chatA) {
+        chatB.send(msg)
+      } else {
+        chatA.send(msg)
+      }
+      return;
+    }
+    if (msg === 'chatA_token') {
+      chatA = conn
+      return;
+    }
+    if (msg === 'chatB_token') {
+      chatB = conn
+    }
+  })
+  conn.on("close", function (code, reason) {
+    console.log("closed closed")
+  })
+  conn.on("error", function (code, reason) {
+    console.log("error error")
+  })
+}).listen(5000)
 module.exports = app;
